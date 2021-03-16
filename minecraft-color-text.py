39b86dd8b78ba5c5fc4ca27c90b92989df62acd3
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-03-16 14:59:56 -0400

    cleaned up minecraft-color-text.py

--- minecraft-color-text.py
+++ minecraft-color-text.py
@@ -2,39 +2,39 @@
 
 import sys
 
+
 def get_color(color):
-    #         1      2       3         4        5       6              7
-    colors = ["red", "gold", "yellow", "green", "blue", "dark_purple", "light_purple"]
-    return "\"color\":\"" + colors[color] + "\"" 
+    colors = ["red",           # 0
+              "gold",          # 1
+              "yellow",        # 2
+              "green",         # 3
+              "blue",          # 4
+              "dark_purple",   # 5
+              "light_purple"]  # 6
+
+    return "\"color\":\"{}\"".format(colors[color])
+
 
 def main():
-    opener = "["
-    prefix = "{\"text\":\""
-    joiner = "\","
-    suffix = "},"
-    closer = "]"
+    color_index = 0
+    word_index = 0
 
-    c_index = 0
+    string = "["
 
-    string = opener
-
-    # Ignore the name of the program in sys.argv
-    program_name = True
-
-    for i in sys.argv:
-        if not program_name:
-            for j in i:
-                string += prefix + j + joiner + get_color(c_index) + suffix
-                if c_index < 6:
-                    c_index += 1
-                else:
-                    c_index = 0
+    for word in sys.argv[1:]:
+        for char in word:
+            string += "{\"text\":\"%s\",%s}," % (char, get_color(color_index))
+            if color_index < 6:
+                color_index += 1
+            else:
+                color_index = 0
+        if word_index < (len(sys.argv) - 2):
             string += "{\"text\":\" \"},"
+            word_index += 1
 
-        program_name = False
-
-    string = string[:-1] + closer
+    string = string[:-1] + "]"
 
     print(string)
+
 
 main()
